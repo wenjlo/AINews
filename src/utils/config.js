cb0d8c40d70d40ae6e0c9e8cfd4bdd19c4321e34
wenjlo77 <wenjlo77@gmail.com>
2025-08-04 17:30:32 +0800

    update cards

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://news-api-app-jpj5c.ondigitalocean.app';
+export const API_URL = 'https://news-api-app-xss6j.ondigitalocean.app';
 // export const API_URL = 'http://127.0.0.1:5000';
